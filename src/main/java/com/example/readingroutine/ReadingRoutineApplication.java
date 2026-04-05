7c520a8e3e3bf47dce3d38a7d150f9d0ebceabd8
gaaaani <geseo0523@gmail.com>
2026-04-05 16:45:08 +0900

    init: project setup

---
+++ src/main/java/com/example/readingroutine/ReadingRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.example.readingroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingRoutineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingRoutineApplication.class, args);
+    }
+
+}
